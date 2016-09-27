7ca21b1aaf1039044169469fada131f776059ba9
JohnathanFlint <nicholas.white142@gmail.com>
2016-09-27 07:54:38 -0600

    Created getters and setters.

--- src/popups/model/PopupsModel.java
+++ src/popups/model/PopupsModel.java
@@ -5,4 +5,45 @@ public class PopupsModel
 	private int number;
 	private double value;
 	private String words;
+	private boolean isValid;
+	
+	public int getNumber()
+	{
+		return number;
+	}
+	
+	public double getValue()
+	{
+		return value;
+	}
+	
+	public String getWords()
+	{
+		return words;
+	}
+	
+	public boolean getIsValid()
+	{
+		return isValid;
+	}
+	
+	public void setNumber(int number)
+	{
+		this.number = number;
+	}
+	
+	public void setValue(double value)
+	{
+		this.value = value;
+	}
+	
+	public void setWords(String words)
+	{
+		this.words = words;
+	}
+	
+	public void set (boolean isValid)
+	{
+		this.isValid = isValid;
+	}
 }
