96dcf3ebcaab2edde5745d39e12a2ba517aa55c7
JohnathanFlint <nicholas.white142@gmail.com>
2016-09-29 08:25:08 -0600

    Created a try/catch statement.

--- src/popups/controller/PopupsController.java
+++ src/popups/controller/PopupsController.java
@@ -14,16 +14,37 @@ public class PopupsController
 	
 	public void start()
 	{
-		display.displayMessage("Jacob the Destroyer of Salt attacks you.");
-		String firstMove = display.collectResponse("Do you run or fight?");
-		if(firstMove .equalsIgnoreCase)
-		/*String answer = "sample";
+		//display.displayMessage("Jacob the Destroyer of Salt attacks you.");
+		//String firstMove = display.collectResponse("Do you run or fight?");
+		//if(firstMove .equalsIgnoreCase)
+		String answer = "sample";
 		while(answer != null && !answer.equals(""))
 		{
 		display.displayMessage("Hi there ;-)");
 		
 		answer = display.collectResponse("What is going on?");
 		display.displayMessage(answer + " is what you said.");
-		}*/
+		}
+	}
+	/**
+	 * Checks if the supplied string can be parsed to a double value.
+	 * @param potentialValue The String to test.
+	 * @return Whether the value could  parsed, false otherwise.
+	 */
+	private boolean isDouble(String potentialValue)
+	{
+		boolean isParseable = false;
+		
+		try
+		{
+			double test = Double.parseDouble(potentialValue);
+			isParseable = true;
+		}
+		catch(NumberFormatException notDouble)
+		{
+			display.displayMessage("This is not a double value >:-(");
+		}
+		
+		return isParseable;
 	}
 }
